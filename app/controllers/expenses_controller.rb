b708a9719aa5155523d998f59963febb847afff9
jhon11390 <meljhon2@gmail.com>
2020-03-29 00:03:34 -0500

    Se arregla el controlador index para que filtre los datos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,16 @@
 class ExpensesController < ApplicationController
   def index
-   @expenses = Expense.order("date DESC")
+    if params[:concept] == "burral"
+      @expenses = Expense.where(category_id: 1)
+      if params[:category_id] == "2" || params[:category_id] == "3"
+        @expenses = Expense.where(category_id: 0)
+      end
+    elsif params[:concept] == "gasolina" && params[:category_id] == "2"
+      @expenses = Expense.where(category_id: 2)
+    elsif params[:category_id] == "3"
+      @expenses = Expense.where(category_id: 3)
+    else
+      @expenses = Expense.order("date DESC")
+    end
   end
 end
